ebd8917bafe600f3444a3b1c9290bd975de28d01
niteen_dhule <ndhule1988@gmail.com>
2022-10-04 23:39:20 +0530

    Added excel and json as dummy data in resource and reading the data from it

--- java-1.8/src/main/java/com/java8/util/ExcelProcessor.java
+++ java-1.8/src/main/java/com/java8/util/ExcelProcessor.java
@@ -1,5 +1,27 @@
 package com.java8.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import javax.annotation.Resource;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.ResourceUtils;
+
 public class ExcelProcessor {
+	
+	
+	
+	public static void loadJsonFile() throws IOException {
+		
+		File resource = ResourceUtils.getFile("classpath:users_details.json"); 
+		System.out.println("FILE : "+resource);
+		String text = new String(Files.readAllBytes(resource.toPath()));
+		System.out.println("JSON STRING :: "+text);
+		
+	}
+	
 
 }
